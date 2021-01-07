ef8de8897829a0c0bd641eecabce0eafd9ae010f
RTiculus <samuelwashere2@gmail.com>
2021-01-07 19:26:17 +0000

    Added Bootstrap to Newsletter, updated SuperUser, setting base.html

--- newsletter/views.py
+++ newsletter/views.py
@@ -4,31 +4,31 @@ news = [
     {
         'author': 'Remzi CI',
         'title': 'First Article',
-        'content': 'New newsletter to the masses, hope you like it. <br>Dance is important to the wellbeing and general happiness of all human beings, <br>providing the sport and socialisation that is required for a balanced living',
+        'content': 'New newsletter to the masses, hope you like it. Dance is important to the wellbeing and general happiness of all human beings, providing the sport and socialisation that is required for a balanced living',
         'date_posted': 'December 16, 2020'
     },
     {
         'author': 'Tomiaz CI',
         'title': '2nd Article',
-        'content': 'New newsletter to the masses, hope you like it. <br>Dance is important to the wellbeing and general happiness of all human beings, <br>providing the sport and socialisation that is required for a balanced living',
+        'content': 'New newsletter to the masses, hope you like it. Dance is important to the wellbeing and general happiness of all human beings, providing the sport and socialisation that is required for a balanced living',
         'date_posted': 'December 18, 2020'
     },
     {
         'author': 'Jendi CI',
         'title': '3rd Article',
-        'content': 'New newsletter to the masses, hope you like it. <br>Dance is important to the wellbeing and general happiness of all human beings, <br>providing the sport and socialisation that is required for a balanced living',
+        'content': 'New newsletter to the masses, hope you like it. Dance is important to the wellbeing and general happiness of all human beings, providing the sport and socialisation that is required for a balanced living',
         'date_posted': 'December 20, 2020'
     },
     {
         'author': 'Fausa CI',
         'title': 'Fourth Article',
-        'content': 'New newsletter to the masses, hope you like it. <br>Dance is important to the wellbeing and general happiness of all human beings, <br>providing the sport and socialisation that is required for a balanced living',
+        'content': 'New newsletter to the masses, hope you like it. Dance is important to the wellbeing and general happiness of all human beings, providing the sport and socialisation that is required for a balanced living',
         'date_posted': 'December 22, 2020'
     },
     {
         'author': 'Grego CI',
         'title': 'Fifth Article',
-        'content': 'New newsletter to the masses, hope you like it. <br>Dance is important to the wellbeing and general happiness of all human beings, <br>providing the sport and socialisation that is required for a balanced living',
+        'content': 'New newsletter to the masses, hope you like it. Dance is important to the wellbeing and general happiness of all human beings, providing the sport and socialisation that is required for a balanced living',
         'date_posted': 'December 24, 2020'
     },
 ]
@@ -42,4 +42,4 @@ def home(request):
 
 
 def director(request):
-        return render(request, 'newsletter/director.html', {'title': 'About'})
+    return render(request, 'newsletter/director.html', {'title': 'About'})
