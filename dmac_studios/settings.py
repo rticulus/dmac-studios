a45e7686b1e96ae77554aff0899adc6782ffe417
RTiculus <samuelwashere2@gmail.com>
2021-01-08 11:57:00 +0000

    Fixed- base.html not working

--- dmac_studios/settings.py
+++ dmac_studios/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dmacstudios.apps.DmacstudiosConfig',
+    'dmacstudios',
+    #'dmacstudios.apps.DmacstudiosConfig',
     'bookings.apps.BookingsConfig',
     'shop.apps.ShopConfig',
     'newsletter.apps.NewsletterConfig',
@@ -62,7 +63,10 @@ ROOT_URLCONF = 'dmac_studios.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- dmacstudios/views.py
+++ dmacstudios/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 
+def index(request):
+    return render(request, 'dmacstudios/index.html')
+
+
 def home(request):
     return render(request, 'dmacstudios/home.html')
 
 
 def about(request):
     return render(request, 'dmacstudios/about.html')
-
-
-def index(request):
-    return render(request, 'dmacstudios/index.html')
