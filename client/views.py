6aa4e002830bf4835b4e36f659ea0b5aca7f0a5c
RTiculus <samuelwashere2@gmail.com>
2021-01-15 09:40:37 +0000

    change login path

--- client/views.py
+++ client/views.py
@@ -4,3 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 def signup(request):
     form = UserCreationForm()
     return render(request, 'client/signup.html', {'form': form})
+
+
+def login(request):
+    form = UserCreationForm()
+    return render(request, 'client/login.html', {'form': form})
