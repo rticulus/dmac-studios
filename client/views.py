1ea62577840c96b1521b09606a24a4733990f447
RTiculus <samuelwashere2@gmail.com>
2021-01-17 21:01:40 +0000

    Tidying up code

--- client/views.py
+++ client/views.py
@@ -9,13 +9,8 @@ def signup(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'DMAC account created for {username}, You must now login to continue')
+            messages.success(request, f'DMAC account created for {username}. You must now login to continue. Thankyou')
             return redirect('dmacstudios-home')
     else:
         form = UserRegisterForm()
     return render(request, 'client/signup.html', {'form': form})
-
-
-def login(request):
-    form = UserRegisterForm()
-    return render(request, 'client/login.html', {'form': form})
