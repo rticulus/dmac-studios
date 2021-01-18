72e307da3c4dca3f46ef85ef0c07cc561a0f03c6
RTiculus <samuelwashere2@gmail.com>
2021-01-18 09:35:38 +0000

    Git clean

--- dmac_studios/settings.py
+++ dmac_studios/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import django_heroku
 import os
 from pathlib import Path
+import psycopg2
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +28,7 @@ SECRET_KEY = '4lxyh-@7#%*cd383mlqo9@#tz3@w92i*s9l*a76$42v#m=k(z)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [dmac-studios.herokuapp.com]
+ALLOWED_HOSTS = [dmacstudios.herokuapp.com]
 
 
 # Application definition
